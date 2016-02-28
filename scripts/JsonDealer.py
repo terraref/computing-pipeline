d256a6ce7cfe39b50c73ef9b0d9a0bf6548d8281
FlyingWithJerome <jiarunm@uci.edu>
2016-02-27 18:46:41 -0800

    this one is the most updated one, the first one is a test

--- scripts/JsonDealer.py
+++ scripts/JsonDealer.py
@@ -7,7 +7,9 @@ Created on Feb 5, 2016
 import json
 from netCDF4 import Dataset
 
-_constructorTemplate = '''self.{var} = source[u'lemnatec_measurement_metadata'][u'{var}']'''
+_constructorTemplate  = '''self.{var} = source[u'lemnatec_measurement_metadata'][u'{var}']'''
+_globalUnitDictionary = {'m':'meter', 's':'second', 'm/s': 'meters second^(-1)', '':''}
+_velocityDictionary   = {'x':'u', 'y':'v', 'z':'w'}
 
 class JsonError(Exception):
    def __init__(self, message):
@@ -23,7 +25,6 @@ class DataContainer(object):
 
    def __str__(self):
       result = str()
-
       for members in self.__dict__:
          result += (str(members)+': '+str(self.__dict__[members])+'\n')
       return result
@@ -37,13 +38,58 @@ class DataContainer(object):
 
       for members in self.__dict__:
          tempGroup = netCDFHandler.createGroup(members)
-         tempGroup.createDimension(members, len(self.__dict__[members]))
          for submembers in self.__dict__[members]:
-            tempValue = tempGroup.createVariable(submembers, str, members)
-            tempValue = self.__dict__[members][submembers]
-            #print submembers, ":", tempValue
+            if not _isdigit(self.__dict__[members][submembers]):
+               setattr(tempGroup, _replaceIllegalChar(submembers), self.__dict__[members][submembers])
+            else:
+               setattr(tempGroup, _replaceIllegalChar(submembers), self.__dict__[members][submembers])
+               nameSet = _spliter(submembers)
+
+               if 'Velocity' in submembers or 'Position' in submembers:
+                  tempVariable = tempGroup.createVariable(nameSet[0][-1], 'f8')
+                  setattr(tempVariable,'long_name', nameSet[0][0])
+                  setattr(tempVariable,'unit',      nameSet[1])
+               else:
+                  tempVariable = tempGroup.createVariable(nameSet[0], 'f8')
+                  setattr(tempVariable,'long_name', nameSet[0])
+
+               tempVariable.assignValue(float(self.__dict__[members][submembers]))
 
       netCDFHandler.close()     
+
+
+def _isdigit(string):
+   try:
+      if '.' in string: float(string)
+      else: int(string)
+      return True
+   except:
+      return False
+
+def _replaceIllegalChar(string):
+   rtn = str()
+   if "current setting" in string: string = string.split(' ')[-1]
+
+   for members in string:
+      if members == '/':   rtn += ' per '
+      elif members == ' ': rtn += '_'
+      else:                rtn += members
+
+   return rtn
+
+def _spliter(string):
+   long_name= str()
+
+   for members in string:
+      if members != '[': long_name += members
+      else: break
+
+   if 'Position' in string:
+      return [_replaceIllegalChar(long_name.strip(' ')), long_name.strip(' ').split(' ')[-1]], _globalUnitDictionary[string[string.find('[')+1: string.find(']')].encode('ascii','ignore')]
+   elif 'Velocity' in string:
+      return [_replaceIllegalChar(long_name.strip(' ')), _velocityDictionary[long_name.strip(' ').split(' ')[-1]]], _globalUnitDictionary[string[string.find('[')+1: string.find(']')].encode('ascii','ignore')]
+   else:
+      return _replaceIllegalChar(long_name.strip(' ')), _replaceIllegalChar(string)
 
 
 def _filteringTheHeadings(target):
@@ -63,19 +109,14 @@ def jsonHandler(jsonFile):
    try:
       with open(jsonFile) as fileHandler:
          for dataMember in fileHandler.readlines():
-            rawData += dataMember.strip('\\').strip('\t').strip('\n')   
+            rawData += dataMember.strip('\\').strip('\t').strip('\n')  
    except Exception as err:
       print 'Fatal Error: ', repr(err)
-    #print len(json.loads(rawData))
    return json.loads(rawData,object_hook=_filteringTheHeadings)    
 
-# def testReader():
-#    testFile = Dataset('test.nc','r')  
-#    print testFile.groups
 
 if __name__ == '__main__':
    testCase = jsonHandler('test.json')
    testCase.writeToNetCDF()
-   testReader()
     
     
