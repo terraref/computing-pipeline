34e663e8632bf58951b768f22272f1a8b0dade0e
Charlie Zender <zender@uci.edu>
2016-03-06 20:25:39 -0800

    add terraref.nco

--- scripts/terraref.sh
+++ scripts/terraref.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
+# Prerequisites:
+# GDAL: sudo aptitude install gdal
+# NCO: sudo aptitude install nco
+# Python: 
+
 # Convert raster to netCDF
 gdal_translate -ot Float32 -of netCDF ${DATA}/terraref/data ${DATA}/terraref/data.nc
 
-# Convert from netCDF3 to netCDF4
+# Convert netCDF3 to netCDF4
 ncks -O -4 ${DATA}/terraref/data.nc ${DATA}/Desktop/terraref/data.nc4
 
 # Add metadata
-ncatted -a "Conventions,global,o,sng,CF-1.5" \
-	-a "Project,global,o,sng,TERRAREF" \
-	${DATA}/terraref/data.nc4
+ncatted -O -a "Conventions,global,o,sng,CF-1.5" -a "Project,global,o,sng,TERRAREF" ${DATA}/terraref/data.nc4
+
 # Parse JSON metadata
 python JsonDealer.py test.json ${DATA}/terraref/test.nc4
+
 # Combine metadata with data
 ncks -A ${DATA}/terraref/test.nc ${DATA}/terraref/data.nc4
