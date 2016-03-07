bba223b1d65980fef1cb89b28721b5f83e3a3646
Charlie Zender <zender@uci.edu>
2016-03-06 20:43:34 -0800

    Start coordinating scripts into workflow

--- scripts/terraref.sh
+++ scripts/terraref.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
+# Purpose: Convert raw imagery from raster to netCDF and annotate with metadata
+
 # Prerequisites:
 # GDAL: sudo aptitude install gdal
 # NCO: sudo aptitude install nco
-# Python: 
+# Python:
+
+# Usage:
+# terraref.sh > ~/terraref.out 2>&1 &
 
 # Convert raster to netCDF
 gdal_translate -ot Float32 -of netCDF ${DATA}/terraref/data ${DATA}/terraref/data.nc
 
 # Convert netCDF3 to netCDF4
 ncks -O -4 ${DATA}/terraref/data.nc ${DATA}/Desktop/terraref/data.nc4
+
+# Combine 2D TR image data into single 3D variable
+# fxm: Currently only works with HMB-20160131-VLIST branch of NCO
+ncap2 -4 -v -O -S ${HOME}/computing-pipline/scripts/terraref.nco ${DATA}/terraref/data.nc4 ${DATA}/terraref/data.nc4
 
 # Add metadata
 ncatted -O -a "Conventions,global,o,sng,CF-1.5" -a "Project,global,o,sng,TERRAREF" ${DATA}/terraref/data.nc4
