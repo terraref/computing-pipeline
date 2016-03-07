a2fb3c602cccef4caeae8fa5f6b3b943a4075d74
Charlie Zender <zender@uci.edu>
2016-03-06 16:47:14 -0800

    UNIX and bash support

--- scripts/terraref.sh
+++ scripts/terraref.sh
@@ -1,8 +1,11 @@
+#!/bin/bash
 
 # Convert raster to netCDF
 gdal_translate -ot Float32 -of netCDF ${DATA}/terraref/data ${DATA}/terraref/data.nc
 
+# Convert from netCDF3 to netCDF4
 ncks -O -4 ${DATA}/terraref/data.nc ${DATA}/Desktop/terraref/data.nc4
+
 # Add metadata
 ncatted -a "Conventions,global,o,sng,CF-1.5" \
 	-a "Project,global,o,sng,TERRAREF" \
