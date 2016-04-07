ee1ff4e8563b74060d73632b1adc1ef616ed6460
FlyingWithJerome <jiarunm@uci.edu>
2016-04-07 10:11:59 -0700

    DataProcess module has now been merged with JsonDealer

--- scripts/hyperspectral/JsonDealer.py
+++ scripts/hyperspectral/JsonDealer.py
@@ -11,14 +11,20 @@ python JsonDealerPath filePath1 filePath2
 
 where
 JsonDealerPath is where this script located
-filePath1      is where the source json file located
+filePath1      is where the source data file located <data_name>
 filePath2      is users' expected output location
+
+Warning:
+Make sure the json metadata ended with <data_name>+_metadata.json and the hdr file ended with <data_name>+_raw.hdr
 
 Example:
 python ${HOME}/terraref/computing-pipeline/scripts/hyperspectral/JsonDealer.py ${DATA}/terraref/test_metadata.json ${DATA}/terraref/test_metadata.nc4
 ----------------------------------------------------------------------------------------
 This script is version-independent, and works with both Python 2 and 3, depending on the netCDF4 module version
 Thanks for the advice from Professor Zender and sample data from Dr. LeBauer.
+----------------------------------------------------------------------------------------
+Update 4.1:
+Merged with DataProcess module; now JsonDealer will do all the jobs.
 ----------------------------------------------------------------------------------------
 '''
 
@@ -27,200 +33,327 @@ import time
 import sys
 import os
 import platform
+import struct
 from netCDF4 import Dataset
 
-_constructorTemplate  = '''self.{var} = source[u'lemnatec_measurement_metadata'][u'{var}']'''
-_globalUnitDictionary = {'m':'meter', 's':'second', 'm/s': 'meter second-1', '':''}
-_velocityDictionary   = {'x':'u', 'y':'v', 'z':'w'}
+_CONSTRUCTOR_TEMPLATE  = '''self.{var} = source[u'lemnatec_measurement_metadata'][u'{var}']'''
+_UNIT_DICTIONARY = {'m': 'meter',
+                    's': 'second', 'm/s': 'meter second-1', '': ''}
+_VELOCITY_DICTIONARY = {'x': 'u', 'y': 'v', 'z': 'w'}
+DATATYPE = {'1': ('H', 2), '2': ('i', 4), '3': ('l', 4), '4': ('f', 4), '5': (
+    'd', 8), '12': ('H', 4), '13': ('L', 4), '14': ('q', 8), '15': ('Q', 8)}
+_RAW_VERSION = platform.python_version()[0]
 
-_rawVersion = platform.python_version()[0]
 
 class JsonError(Exception):
-   '''
-   User-Defined Error Class
-   '''
-   def __init__(self, message):
-      self.message = message
-        
-   def __str__(self):
-      return repr(self.message)
+    '''
+    User-Defined Error Class
+    '''
+
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return repr(self.message)
+
+
+class TimeMeasurement(object):
+    '''
+    Supportive class;
+    Measuring the time used by unpacking the data
+    '''
+
+    def __init__(self, lineName):
+        self.lineName = lineName
+
+    def __enter__(self):
+        self.startTime = time.time()
+
+    def __exit__(self, *args):
+        self.endTime = time.time()
+        self.runningTime = (self.endTime - self.startTime) * 1000
+
+        reportHandler = open("PerformanceReport.txt", "w")
+
+        prompt = "%s elapsed time: %.3fms, %.5fs" % (self.lineName,
+                                                     self.runningTime,
+                                                     self.runningTime / 1000)
+        reportHandler.write(prompt)
+        print(prompt)
+
 
 class DataContainer(object):
-   '''
-   A class which saves the data from Json file
-   '''
-   def __init__(self,source):
-      for members in source[u'lemnatec_measurement_metadata']:
-         exec(_constructorTemplate.format(var=members))
+    '''
+    A class which saves the data from Json file
+    '''
 
-   def __str__(self):
-      result = str()
-      for members in self.__dict__:
-         result += (str(members)+': '+str(self.__dict__[members])+'\n')
-      return result
+    def __init__(self, source):
+        for members in source[u'lemnatec_measurement_metadata']:
+            exec(_CONSTRUCTOR_TEMPLATE.format(var=members))
 
-   def __getitem__(self, param):
-      if param in self.__dict__:
-         return self.__dict__[param]
+    def __str__(self):
+        result = str()
+        for members in self.__dict__:
+            result += (str(members) + ': ' +
+                       str(self.__dict__[members]) + '\n')
+        return result
 
-   def writeToNetCDF(self, filePath, commandLine):
+    def __getitem__(self, param):
+        if param in self.__dict__:
+            return self.__dict__[param]
 
-      netCDFHandler = _fileExistingCheck(filePath, self)
+    def writeToNetCDF(self, inputFilePath, outputFilePath, commandLine):
+        setattr(self,"HeaderInfo",None) #weird, but useful to check whether the HeaderInfo id in the netCDF file
+        netCDFHandler = _fileExistingCheck(outputFilePath, self)
+        delattr(self,"HeaderInfo")
 
-      if netCDFHandler == 0:
-         return
 
-      for members in self.__dict__:
-         tempGroup = netCDFHandler.createGroup(members)
-         for submembers in self.__dict__[members]:
-            if not isDigit(self.__dict__[members][submembers]):
-               setattr(tempGroup, _replaceIllegalChar(submembers), 
-                        self.__dict__[members][submembers])
+        if netCDFHandler == 0:
+            return
 
-            else:
-               setattr(tempGroup, _replaceIllegalChar(submembers), 
-                        self.__dict__[members][submembers])
-               nameSet = _spliter(submembers)
+        for members in self.__dict__:
+            tempGroup = netCDFHandler.createGroup(members)
+            for submembers in self.__dict__[members]:
+                if not isDigit(self.__dict__[members][submembers]):
+                    setattr(tempGroup, _replaceIllegalChar(submembers),
+                            self.__dict__[members][submembers])
 
-               if 'Velocity' in submembers or 'Position' in submembers:
-                  tempVariable = tempGroup.createVariable(nameSet[0][-1], 'f8')
-                  setattr(tempVariable,'long_name', nameSet[0][0])
-                  setattr(tempVariable,'unit',      nameSet[1])
-               else:
-                  tempVariable = tempGroup.createVariable(nameSet[0], 'f8')
-                  setattr(tempVariable,'long_name', nameSet[0])
+                else:
+                    setattr(tempGroup, _replaceIllegalChar(submembers),
+                            self.__dict__[members][submembers])
+                    nameSet = _spliter(submembers)
 
-               tempVariable.assignValue(float(self.__dict__[members][submembers]))
+                    if 'Velocity' in submembers or 'Position' in submembers:
+                        tempVariable = tempGroup.createVariable(
+                            nameSet[0][-1], 'f8')
+                        setattr(tempVariable, 'long_name', nameSet[0][0])
+                        setattr(tempVariable, 'unit',      nameSet[1])
+                    else:
+                        tempVariable = tempGroup.createVariable(
+                            nameSet[0], 'f8')
+                        setattr(tempVariable, 'long_name', nameSet[0])
 
-      netCDFHandler.history = _timeStamp()+' : python '+commandLine
+                    tempVariable.assignValue(
+                        float(self.__dict__[members][submembers]))
+        writeHeaderFile(inputFilePath, netCDFHandler)
 
-      netCDFHandler.close()  
+        netCDFHandler.history = _timeStamp() + ' : python ' + commandLine
+
+        netCDFHandler.close()
+
+
+def getDimension(fileName):
+    '''
+    Acquire dimensions from related HDR file
+    '''
+    fileHandler = open(fileName + '_raw.hdr')
+
+    for members in fileHandler.readlines():
+        if "samples" in members:
+            x = members[members.find("=") + 1:len(members)]
+        elif "lines" in members:
+            y = members[members.find("=") + 1:len(members)]
+        elif "bands" in members:
+            wavelength = members[members.find("=") + 1:len(members)]
+
+    return int(wavelength.strip('\n').strip('\r')), int(x.strip('\n').strip('\r')), int(y.strip('\n').strip('\r'))
+
+
+def getWavelength(fileName):
+    '''
+    Acquire wavelength(s) from related HDR file
+    '''
+    fileHandler = open(fileName + '_raw.hdr')
+    wavelengthGroup = [float(x.strip('\r').strip('\n').strip(',')) for x in fileHandler.readlines()
+                       if isDigit(x.strip('\r').strip('\n').strip(','))]
+
+    return wavelengthGroup
+
+
+def getHeaderInfo(fileName):
+    '''
+    Acquire Other Information from related HDR file
+    '''
+    fileHandler, infoDictionary = open(fileName + '_raw.hdr'), dict()
+    for members in fileHandler.readlines():
+        if '=' in members and 'wavelength' not in members:
+            infoDictionary[members[0:members.find(
+                "=") - 1]] = members[members.find("=") + 2:].strip('\n').strip('\r')
+
+    return infoDictionary
 
 
 def _fileExistingCheck(filePath, dataContainer):
-   '''
-   This method checks whether filePath has same variable name as dataContainer. 
-   If so, user decides whether skip or overwrite it (no append, since netCDF does not support the repeating variable names)
+    '''
+    This method will check wheter the filePath has the same variable name as the dataContainer has. If so,
+    user will decide whether skip or overwrite it (no append,
+    since netCDF does not support the repeating variable names)
 
-   Private to module members
-   '''
-   userPrompt = 'Output file already exists; skip it or overwrite or append? (S, O, A)'
+    Private to module members
+    '''
+    userPrompt = 'Output file already exists; skip it or overwrite or append? (S, O, A)'
 
-   if os.path.exists(filePath):
-      netCDFHandler = Dataset(filePath,'r',format='NETCDF4')
-      if set([x.encode('utf-8') for x in netCDFHandler.groups]) - \
-         set([x for x in dataContainer.__dict__]) != set([x.encode('utf-8') for x in netCDFHandler.groups]):
+    if os.path.exists(filePath):
+        netCDFHandler = Dataset(filePath, 'r', format='NETCDF4')
+        if set([x.encode('utf-8') for x in netCDFHandler.groups]) - \
+           set([x for x in dataContainer.__dict__]) != set([x.encode('utf-8') for x in netCDFHandler.groups]):
 
-         while True:
-            if _rawVersion == '2':
-               exec("userChoice = str(raw_input(userPrompt))")
-            else:
-               exec("userChoice = str(input(userPrompt))")
+            while True:
+                if _RAW_VERSION == '2':
+                    exec("userChoice = str(raw_input(userPrompt))")
+                else:
+                    exec("userChoice = str(input(userPrompt))")
 
-            if userChoice is 'S':
-               return 0
-            elif userChoice is 'O' and 'A':
-               os.remove(filePath)
-               return Dataset(filePath,'w',format='NETCDF4')
+                if userChoice is 'S':
+                    return 0
+                elif userChoice is 'O' and 'A':
+                    os.remove(filePath)
+                    return Dataset(filePath, 'w', format='NETCDF4')
 
-   else:
-      return Dataset(filePath,'w',format='NETCDF4')
+    else:
+        return Dataset(filePath, 'w', format='NETCDF4')
 
 
 def isDigit(string):
-   '''
-   This method checks whether string can convert to int or float
-   Similar to .isdigit method in built-in string class, but Python's will not check whether it is a float
+    '''
+    This method will check whether the string can be convert to int or float
+    Similar to .isdight method in built-in string class, but python's will not check whether it is a float
 
-   Private to module members
-   '''
-   try:
-      if '.' in string: float(string)
-      else: int(string)
-      return True
-   except:
-      return False
+    Public to other modules
+    '''
+    try:
+        if '.' in string:
+            float(string)
+        else:
+            int(string)
+        return True
+    except:
+        return False
+
 
 def _replaceIllegalChar(string):
-   '''
-   This method replaces spaces (' '), slashes('/')
+    '''
+    This method will replace spaces (' '), slashes('/')
 
-   Private to module members
-   '''
-   rtn = str()
-   if "current setting" in string: string = string.split(' ')[-1]
+    Private to module members
+    '''
+    rtn = str()
+    if "current setting" in string:
+        string = string.split(' ')[-1]
 
-   for members in string:
-      if members == '/':   rtn += ' per '
-      elif members == ' ': rtn += '_'
-      else:                rtn += members
+    for members in string:
+        if members == '/':
+            rtn += ' per '
+        elif members == ' ':
+            rtn += '_'
+        else:
+            rtn += members
 
-   return rtn
+    return rtn
+
 
 def _spliter(string):
-   '''
-   This method parses the string to a group of long names, short names and values
-   Position and Velocity variables will be specially treated
+    '''
+    This method will parse the string to a group of long names, short names and values
+    Position and Velocity variables will be specially treated
 
-   Private to module members
-   '''
-   long_name= str()
+    Private to module members
+    '''
+    long_name = str()
 
-   for members in string:
-      if members != '[': long_name += members
-      else: break
+    for members in string:
+        if members != '[':
+            long_name += members
+        else:
+            break
 
-   if 'Position' in string:
-      return [_replaceIllegalChar(long_name.strip(' ')),\
-               long_name.strip(' ').split(' ')[-1]]\
-               ,_globalUnitDictionary[string[string.find('[')+1: \
-               string.find(']')].encode('ascii','ignore')]
+    if 'Position' in string:
+        return [_replaceIllegalChar(long_name.strip(' ')),
+                long_name.strip(' ').split(' ')[-1]]\
+            , _UNIT_DICTIONARY[string[string.find('[') + 1:
+                                      string.find(']')].encode('ascii', 'ignore')]
 
-   elif 'Velocity' in string:
-      return [_replaceIllegalChar(long_name.strip(' ')), 
-              _velocityDictionary[long_name.strip(' ').split(' ')[-1]]]\
-              ,_globalUnitDictionary[string[string.find('[')+1: \
-              string.find(']')].encode('ascii','ignore')]
+    elif 'Velocity' in string:
+        return [_replaceIllegalChar(long_name.strip(' ')),
+                _VELOCITY_DICTIONARY[long_name.strip(' ').split(' ')[-1]]]\
+            , _UNIT_DICTIONARY[string[string.find('[') + 1:
+                                      string.find(']')].encode('ascii', 'ignore')]
 
-   else:
-      return _replaceIllegalChar(long_name.strip(' '))\
-             ,_replaceIllegalChar(string)
+    else:
+        return _replaceIllegalChar(long_name.strip(' '))\
+            , _replaceIllegalChar(string)
 
 
 def _filteringTheHeadings(target):
-   '''
-   Hook for JSON module to filter and process useful data
+    '''
+    A hook for json module to filter and process the useful data
 
-   Private to module members
-   '''
-   if u'lemnatec_measurement_metadata' in target:
-      return DataContainer(target)
-   return target
+    Private to module members
+    '''
+    if u'lemnatec_measurement_metadata' in target:
+        return DataContainer(target)
+    return target
+
 
 def _timeStamp():
-   '''
-   acquire the time from std time module and return a well-formatted timestamp
-   '''
-   return time.strftime("%a %b %d %H:%M:%S %Y",  time.localtime(int(time.time())))
+    return time.strftime("%a %b %d %H:%M:%S %Y",  time.localtime(int(time.time())))
+
 
 def jsonHandler(jsonFile):
-   '''
-   pass JSON object to built-in JSON module
-   '''
-   rawData = str()
+    '''
+    pass the json object to built-in json module
+    '''
+    rawData = str()
 
-   try:
-      with open(jsonFile) as fileHandler:
-         for dataMember in fileHandler.readlines():
-            rawData += dataMember.strip('\\').strip('\t').strip('\n')  
-   except Exception as err:
-      print 'Fatal Error: ', repr(err)
-   return json.loads(rawData,object_hook=_filteringTheHeadings)    
+    try:
+        with open(jsonFile + '_metadata.json') as fileHandler:
+            for dataMember in fileHandler.readlines():
+                rawData += dataMember.strip('\\').strip('\t').strip('\n')
+    except Exception as err:
+        print('Fatal Error: ', repr(err))
+    return json.loads(rawData, object_hook=_filteringTheHeadings)
+
+
+def writeHeaderFile(fileName, netCDFHandler):
+    '''
+    The main function, reading the data and exporting netCDF file
+    '''
+    dimensionWavelength, dimensionX, dimensionY = getDimension(fileName)
+    wavelength, hdrInfo = getWavelength(fileName), getHeaderInfo(fileName)
+
+    # netCDFHandler.createDimension('wavelength',       dimensionWavelength)
+    # netCDFHandler.createDimension('x',          dimensionX)
+    # netCDFHandler.createDimension('y',          dimensionY)
+    # netCDFHandler.createDimension('wavelength', len(wavelength))
+
+    # mainDataHandler, tempVariable = open(fileName + '_raw'), netCDFHandler.createVariable(
+    #     'exposure_2', 'f8', ('band', 'x', 'y'))  # ('band', 'x', 'y')
+    fileSize = os.path.getsize(fileName + '_raw')
+    dataNumber, dataType, dataSize = fileSize / DATATYPE[hdrInfo['data type']][-1], DATATYPE[hdrInfo['data type']][0],\
+        DATATYPE[hdrInfo['data type']][-1]
+
+    # with TimeMeasurement("unpacking") as lineTiming: #measuring the time
+    # value =
+    # struct.unpack(dataType*dataNumber,mainDataHandler.read(dataSize*dataNumber))#reading
+    # the data from the file
+
+    # with TimeMeasurement("assigning value") as lineTiming:
+    # tempVariable[:,:,:] = value #TODO need a better method to assign value
+    # to avoid "de-interleaving"
+
+    setattr(netCDFHandler, 'wavelength', wavelength)
+    headerInfo = netCDFHandler.createGroup("HeaderInfo")
+
+    for members in hdrInfo:
+        setattr(headerInfo, members, hdrInfo[members])
+        if isDigit(hdrInfo[members]):
+            tempVariable = headerInfo.createVariable(members, 'i4')
+            tempVariable.assignValue(int(hdrInfo[members]))
+            print(hdrInfo[members])
 
 
 if __name__ == '__main__':
-   fileInput, fileOutput = sys.argv[1], sys.argv[2]
+    fileInput, fileOutput = sys.argv[1], sys.argv[2]
 
-   testCase = jsonHandler(fileInput)
-   testCase.writeToNetCDF(fileOutput,fileInput+' '+fileOutput)
-   
+    testCase = jsonHandler(fileInput)
+    testCase.writeToNetCDF(fileInput, fileOutput, fileInput + ' ' + fileOutput)
