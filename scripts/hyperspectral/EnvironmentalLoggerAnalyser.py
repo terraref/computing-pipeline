a4885e8ab58f82ea1d3319ae53e842d532dd31a6
FlyingWithJerome <jiarunm@uci.edu>
2016-04-28 21:11:41 -0700

    Fix errors in assigning values and rawValues, as well as the structure of output files

--- scripts/hyperspectral/EnvironmentalLoggerAnalyser.py
+++ scripts/hyperspectral/EnvironmentalLoggerAnalyser.py
@@ -16,7 +16,7 @@ Usage (in bash):
 python ${HOME}/terraref/computing-pipeline/scripts/hyperspectral/EnvironmentalLoggerAnalyser.py ${DATA}/terraref/input ${DATA}/terraref/output
 
 Please note EnvironmentalLoggerAnalyser.py will take the second parameter as the input folder (containing JSON files,
-but it can also be one single file) and the third parameter as the output folder (will dump netCDF files here). 
+but it can also be one single file) and the third parameter as the output folder (will dump netCDF files here).
 If the output folder does not exist, EnvironmentalLoggerAnalyser.py will create one for you.
 
 ----------------------------------------------------------------------------------------
@@ -33,155 +33,157 @@ import sys
 import os
 from netCDF4 import Dataset
 
-_UNIT_DICTIONARY = {u'm': 'meter', u"hPa": "hecto-Pascal", u"DegCelsius" : "Celsius",
-                    u's': 'second', u'm/s': 'meter second-1', u"mm/h" : 'milimeters hour-1',
-                    u"relHumPerCent" : "PerCent", u"?mol/(m^2*s)": "micromole meters-2 second-1",
+_UNIT_DICTIONARY = {u'm': 'meter', u"hPa": "hecto-Pascal", u"DegCelsius": "Celsius",
+                    u's': 'second', u'm/s': 'meter second-1', u"mm/h": 'milimeters hour-1',
+                    u"relHumPerCent": "PerCent", u"?mol/(m^2*s)": "micromole meters-2 second-1",
                     u'kilo Lux': 'kilo Lux', u'degrees': 'degrees', '': ''}
 _NAMES = {'sensor par': 'Sensor Photosynthetical Active Radiation'}
 
 
 def formattingTheInputJSONFile(fileLocation):
-	'''
-	This function will format the source JSON file including multiple JSON objects
-	into a file of JSON array
-	'''
+    '''
+    This function will format the source JSON file including multiple JSON objects
+    into a file of JSON array
+    '''
+    with open(fileLocation, 'r+') as fileHandler:
+        tempList, linePosition = fileHandler.read().split('\n'), list()
+        for i in range(len(tempList)):
+            if "environment_sensor_set_reading" in tempList[i] and i > 2:
+                linePosition.append(i - 1)
 
-	with open(fileLocation, 'r+') as fileHandler:
-		tempList, linePosition = fileHandler.read().split('\n'), list()
-		for i in range(len(tempList)):
-			if "environment_sensor_set_reading" in tempList[i] and i > 2:
-				linePosition.append(i-1)
+        for line in linePosition:
+            del tempList[line]
+            tempList.insert(line, "},{")
 
-		for line in linePosition:
-			del tempList[line]
-			tempList.insert(line, "},{")
+        fileHandler.seek(0)
+        fileHandler.truncate()
 
-		fileHandler.seek(0)
-		fileHandler.truncate()
-
-		if '[' not in tempList[0] and ']' not in tempList[-1]:
-			tempList.insert(0, '[')
-			tempList.append(']')
-			fileHandler.write('\n'.join(tempList))
-		else:
-			fileHandler.write('\n'.join(tempList))
+        if '[' not in tempList[0] and ']' not in tempList[-1]:
+            tempList.insert(0, '[')
+            tempList.append(']')
+            fileHandler.write('\n'.join(tempList))
+        else:
+            fileHandler.write('\n'.join(tempList))
 
 
 def JSONHandler(fileLocation):
-	'''
-	Main JSON handler, write JSON file to a Python list with standard JSON module
-	'''
-
-	JSONMasterList = list()
-
-	#if not os.path.isdir(fileLocation):
-	formattingTheInputJSONFile(fileLocation)
-	with open(fileLocation, 'r') as fileHandler:
-		return json.loads(fileHandler.read())
+    '''
+    Main JSON handler, write JSON file to a Python list with standard JSON module
+    '''
+    JSONMasterList = list()
+    # if not os.path.isdir(fileLocation):
+    formattingTheInputJSONFile(fileLocation)
+    with open(fileLocation, 'r') as fileHandler:
+        return json.loads(fileHandler.read())
 
 
 def renameTheValue(name):
-	'''
-	Rename the value so it becomes legal in netCDF
-	'''
+    '''
+    Rename the value so it becomes legal in netCDF
+    '''
+    if type(name) is unicode:
+        name = name.encode('ascii', 'ignore')
+    if name in _UNIT_DICTIONARY:
+        name = _UNIT_DICTIONARY[name]
+    elif name in _NAMES:
+        name = _NAMES[name]
 
-	if type(name) is unicode:
-		name = name.encode('ascii', 'ignore')
-	if name in _UNIT_DICTIONARY:
-		name = _UNIT_DICTIONARY[name]
-	elif name in _NAMES:
-		name =  _NAMES[name]
+    returningString = str()
+    for letters in name:
+        if letters == ' ':
+            returningString += '_'
+        else:
+            returningString += letters
+    return returningString
 
-	returningString = str()
-	for letters in name:
-		if letters == ' ':
-			returningString += '_'
-		else:
-			returningString += letters
-	return returningString
 
 def getSpectrometerInformation(arrayOfJSON):
-	'''
-	Collect information from spectrometer with special care
-	'''
+    '''
+    Collect information from spectrometer with special care
+    '''
+    maxFixedIntensity = [int(intensityMembers["spectrometer"]["maxFixedIntensity"]) for intensityMembers in
+                         arrayOfJSON]
+    integrationTime = [int(integrateMembers["spectrometer"]["integration time in ?s"]) for integrateMembers in
+                       arrayOfJSON]
 
-	maxFixedIntensity = [int(intensityMembers["spectrometer"]["maxFixedIntensity"]) for intensityMembers in\
-						arrayOfJSON]
-	integrationTime   = [int(integrateMembers["spectrometer"]["integration time in ?s"]) for integrateMembers in\
-						arrayOfJSON]
+    # TODO Reformat the spectrometer[bands]; illegal JSON formatting
 
-	#TODO Reformat the spectrometer[bands]; illegal JSON formatting
-
-	return maxFixedIntensity, integrationTime
+    return maxFixedIntensity, integrationTime
 
 
 def getListOfValue(arrayOfJSON, dataName):
-	'''
-	Collect data from JSON objects which have "value" member
-	'''
-
-	return [float(valueMembers[dataName]['value'].encode('ascii', 'ignore')) for valueMembers in arrayOfJSON]
+    '''
+    Collect data from JSON objects which have "value" member
+    '''
+    return [float(valueMembers[dataName]['value'].encode('ascii', 'ignore')) for valueMembers in arrayOfJSON]
 
 
 def getListOfRawValue(arrayOfJSON, dataName):
-	'''
-	Collect data from JSON objects which have "rawValue" member
-	'''
-
-	return [float(valueMembers[dataName]['rawValue'].encode('ascii', 'ignore')) for valueMembers in arrayOfJSON]
+    '''
+    Collect data from JSON objects which have "rawValue" member
+    '''
+    return [float(valueMembers[dataName]['rawValue'].encode('ascii', 'ignore')) for valueMembers in arrayOfJSON]
 
 
 def main(JSONArray, outputFileName):
-	'''
-	Main netCDF handler, write data to the netCDF file indicated.
-	'''
+    '''
+    Main netCDF handler, write data to the netCDF file indicated.
+    '''
+    netCDFHandler = Dataset(outputFileName, 'w', format='NETCDF4')
+    dataMemberList = [JSONMembers[u"environment_sensor_set_reading"]
+                      for JSONMembers in JSONArray]
+    timeStampList = [JSONMembers[u'timestamp']
+                     for JSONMembers in dataMemberList]
+    timeDimension = netCDFHandler.createDimension("time", None)
+    tempTimeVariable = netCDFHandler.createVariable('time', str, ('time',))
+    for i in range(len(timeStampList)):  # Assign Times
+        tempTimeVariable[i] = timeStampList[i]
 
-	netCDFHandler  = Dataset(outputFileName, 'w', format='NETCDF4')
-	dataMemberList = [JSONMembers[u"environment_sensor_set_reading"] for JSONMembers in JSONArray]
-	timeStampList  = [JSONMembers[u'timestamp'] for JSONMembers in dataMemberList]
-	timeDimension  = netCDFHandler.createDimension("time", None)
-	tempTimeVariable = netCDFHandler.createVariable('time', str, ('time',))
-	for i in range(len(timeStampList)): #Assign Times
-		tempTimeVariable[i] = timeStampList[i]
+    for data in dataMemberList[0]:
+        if data != 'spectrometer' and type(dataMemberList[0][data]) not in (str, unicode):
+            tempVariable = netCDFHandler.createVariable(
+                renameTheValue(data), 'f8', ('time',))
+            tempVariable[:] = getListOfValue(
+                dataMemberList, data)  # Assign "values"
+            if 'unit' in dataMemberList[0][data]:  # Assign Units
+                setattr(tempVariable, 'units', _UNIT_DICTIONARY[
+                        dataMemberList[0][data]['unit']])
+            if 'rawValue' in dataMemberList[0][data]:  # Assign "rawValues"
+                netCDFHandler.createVariable(renameTheValue(data) + '_rawValue', 'f8', ('time',))[:] =\
+                    getListOfRawValue(dataMemberList, data)
+        elif type(dataMemberList[0][data]) in (str, unicode):
+        	netCDFHandler.createVariable(renameTheValue(data), str)[0] = dataMemberList[0][data]
 
-	for data in dataMemberList[0]: 
-		tempGroup = netCDFHandler.createGroup(renameTheValue(data))
-		if 'unit' in dataMemberList[0][data]: #Assign Units
-			tempVariable = tempGroup.createVariable('unit', str)
-			tempVariable[0] = _UNIT_DICTIONARY[dataMemberList[0][data]['unit']]
-			setattr(tempGroup, 'unit', _UNIT_DICTIONARY[dataMemberList[0][data]['unit']])
-		if 'value' in dataMemberList[0][data]: #Assign "values"
-			tempGroup.createVariable('value', 'f8', ('time',))[:] = getListOfValue(dataMemberList, data)
-		if 'rawValue' in dataMemberList[0][data]: #Assign "rawValues"
-			tempGroup.createVariable('rawValue', 'f8', ('time',))[:] = getListOfValue(dataMemberList, data)
-		if data == 'spectrometer': #Special care for spectrometers :)
-			tempGroup.createVariable('maxFixedIntensity', 'f8', ('time',))[:] =\
-			 						getSpectrometerInformation(dataMemberList)[0]
-			tempGroup.createVariable('Integration_Time_In_Microseconds', 'f8', ('time',))[:] =\
-			 						getSpectrometerInformation(dataMemberList)[0]
+        # if data == 'spectrometer': #Special care for spectrometers :)
+        # 	tempGroup.createVariable('maxFixedIntensity', 'f8', ('time',))[:] =\
+        # 	 						getSpectrometerInformation(dataMemberList)[0]
+        # 	tempGroup.createVariable('Integration_Time_In_Microseconds', 'f8', ('time',))[:] =\
+        # 	 						getSpectrometerInformation(dataMemberList)[0]
 
-	for DataMembers in dataMemberList:
-		for subDataMembers in DataMembers:
-			tempGroup = netCDFHandler.createGroup(renameTheValue(subDataMembers))
-			if type(DataMembers[subDataMembers]) is not dict and subDataMembers != 'timestamp':
-				setattr(tempGroup, renameTheValue(subDataMembers), DataMembers[subDataMembers])
+    # for DataMembers in dataMemberList:
+    # 	for subDataMembers in DataMembers:
+    # 		tempGroup = netCDFHandler.createGroup(renameTheValue(subDataMembers))
+    # 		if type(DataMembers[subDataMembers]) is not dict and subDataMembers != 'timestamp':
+    # 			setattr(tempGroup, renameTheValue(subDataMembers), DataMembers[subDataMembers])
 
-	netCDFHandler.close()
+    netCDFHandler.close()
 
 
 if __name__ == '__main__':
-	fileInputLocation, fileOutputLocation = sys.argv[1], sys.argv[2]
-	if not os.path.exists(fileOutputLocation):
-		os.mkdir(fileOutputLocation) #Create folder
+    fileInputLocation, fileOutputLocation = sys.argv[1], sys.argv[2]
+    if not os.path.exists(fileOutputLocation):
+        os.mkdir(fileOutputLocation)  # Create folder
 
-
-	if not os.path.isdir(fileInputLocation):
-		tempJSONMasterList = JSONHandler(fileInputLocation)
-		main(tempJSONMasterList, fileInputLocation.strip('.json')+'.nc')
-	else:
-		for filePath, fileDirectory, fileName in os.walk(fileInputLocation): #Read and Export netCDF to folder
-			for members in fileName:
-				if os.path.join(filePath, members).endswith('.json'):
-					outputFileName     = members.strip('.json') + '.nc'
-					tempJSONMasterList = JSONHandler(os.path.join(filePath, members))
-					main(tempJSONMasterList, os.path.join(fileOutputLocation, outputFileName))
+    if not os.path.isdir(fileInputLocation):
+        tempJSONMasterList = JSONHandler(fileInputLocation)
+        main(tempJSONMasterList, fileInputLocation.strip('.json') + '.nc')
+    else:
+        # Read and Export netCDF to folder
+        for filePath, fileDirectory, fileName in os.walk(fileInputLocation):
+            for members in fileName:
+                if os.path.join(filePath, members).endswith('.json'):
+                    outputFileName = members.strip('.json') + '.nc'
+                    tempJSONMasterList = JSONHandler(
+                        os.path.join(filePath, members))
+                    main(tempJSONMasterList, os.path.join(
+                        fileOutputLocation, outputFileName))
