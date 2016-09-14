4b9b97f3f754687cc80479562299bd5dd99c5431
Max Burnette <mburnet88@gmail.com>
2016-09-14 09:48:00 -0500

    Correct file lock

--- scripts/globusmonitor/globus_monitor_service.py
+++ scripts/globusmonitor/globus_monitor_service.py
@@ -122,7 +122,7 @@ def lockFile(f):
         try:
             fcntl.flock(f, fcntl.LOCK_EX | fcntl.LOCK_NB)
             return
-        except {BlockingIOError, IOError} as e:
+        except (BlockingIOError, IOError) as e:
             # Try again in 1/10th of a second
             time.sleep(0.1)
 
