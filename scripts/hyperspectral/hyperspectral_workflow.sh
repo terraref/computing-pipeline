c9c41d29ae0545de59734915a312c1df10be1a8c
Charlie Zender <zender@uci.edu>
2017-01-13 11:25:13 -0800

    Get sensor type from metadata file not wvl_nbr

--- scripts/hyperspectral/hyperspectral_workflow.sh
+++ scripts/hyperspectral/hyperspectral_workflow.sh
@@ -517,14 +517,14 @@ for ((fl_idx=0;fl_idx<${fl_nbr};fl_idx++)); do
 	ntl_in=$(grep '^interleave' ${hdr_fl} | cut -d ' ' -f 3 | tr -d '\015')
 	typ_in_ENVI=$(grep '^data type' ${hdr_fl} | cut -d ' ' -f 4 | tr -d '\015')
 	xps_tm=$(grep 'current setting exposure' ${mtd_fl} | cut -d ':' -f 2 | tr -d '" ,\015' )
+	sns_nm=$(grep 'sensor product name' ${mtd_fl} | cut -d ':' -f 2 | tr -d '" ,\015' )
 	fl_clb="${drc_spt}/calibration_vnir_${xps_tm}ms.nc"
-	if [ "${wvl_nbr}" -eq 272 ] || [ "${wvl_nbr}" -eq 273 ]; then 
+	if [ "${sns_nm}" = 'SWIR' ]; then 
 	    flg_swir='Yes' # [flg] SWIR camera
-	elif [ "${wvl_nbr}" -eq 955 ]; then 
+	elif [ "${sns_nm}" = 'VNIR' ]; then 
 	    flg_vnir='Yes' # [flg] VNIR camera
 	else
-	    echo "ERROR: Unable to identify camera type (SWIR or VNIR?)"
-	    echo "HINT: ${spt_nm} requires header file ${fl_hdr} to report either 272 or 273 (for SWIR) or 955 (for VNIR) wavelengths. Actual number reported = wvl_nbr = ${wvl_nbr}."
+	    echo "ERROR: metadata file ${fl_mtd} reports unknown camera type ${sns_nm} (not SWIR or VNIR)"
 	    exit 1
 	fi # !wvl_nbr
 	case "${typ_in_ENVI}" in
