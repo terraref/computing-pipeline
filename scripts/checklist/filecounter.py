abfcb581f7beb215a5be33b10d22cd8dfa2b15ce
Max Burnette <mburnet2@illinois.com>
2018-10-04 12:19:34 -0500

    update filecounter, Docker, Readme

--- scripts/checklist/filecounter.py
+++ scripts/checklist/filecounter.py
@@ -2,22 +2,72 @@ import os
 import pandas as pd
 import datetime
 import psycopg2
-from .counter_config import dbname, user, host, password
-
-stereotop_dir = '/data/terraref/sites/ua-mac/raw_data/stereoTop/'
-rgb_geotiff_dir = '/data/terraref/sites/ua-mac/Level_1/rgb_geotiff/'
-flir_ir_dir = '/data/terraref/sites/ua-mac/raw_data/flirIrCamera/'
-ir_geotiff_dir = '/terraref/sites/ua-mac/Level_1/ir_geotiff/'
+import re
+from collections import OrderedDict
 
 
-# TODO make these refer to os environ
+"""
+Dictionary of count definitions for various sensors.
 
-rgb_geotiff_csv = '2018 Pipeline Status - rgb_geotiff.csv'
-ir_geotiff_csv = '2018 Pipeline Status - ir_geotiff.csv'
-check_table_csv = '2018 Pipeline Status - CHECK_TABLE.csv'
+Types:
+    timestamp:  count timestamp directories in each date directory
+    psql:       count rows returned from specified postgres query
+    regex:      count files within each date directory that match regex
+Other fields:
+    path:       path containing date directories for timestamp or regex counts
+    regex:      regular expression to execute on date directory for regex counts
+    query:      postgres query to execute for psql counts
+    parent:     previous count definition for % generation (e.g. bin2tif's parent is stereoTop)
+"""
+sites_root = "/home/clowder/"
+count_defs = {
+    "stereoTop": OrderedDict([
+        ("stereoTop", {
+            "path": os.path.join(sites_root, 'sites/ua-mac/raw_data/stereoTop/'),
+            "type": 'timestamp'}),
+        ("bin2tif", {
+            "path": os.path.join(sites_root, 'sites/ua-mac/Level_1/rgb_geotiff/'),
+            "type": 'timestamp',
+            "parent": "stereoTop"}),
+        ("rulechecker", {
+            "type": "psql",
+            "query": "select count(distinct file_path) from extractor_ids where output like 'Full Field -- RGB GeoTIFFs - %s%%';",
+            "parent": "bin2tif"}),
+        ("fullfield", {
+            "path": os.path.join(sites_root, 'sites/ua-mac/Level_1/fullfield/'),
+            "type": 'regex',
+            "regex": "^.*\d+_rgb_.*thumb.tif"}),
+        ("canopycover", {
+            "path": os.path.join(sites_root, 'sites/ua-mac/Level_1/fullfield/'),
+            "type": 'regex',
+            "regex": '',
+            "parent": "fullfield"})
+    ]),
+    "flirIrCamera": OrderedDict([
+        ("flirIrCamera", {
+            "path": os.path.join(sites_root, 'sites/ua-mac/raw_data/flirIrCamera/'),
+            "type": 'timestamp'}),
+        ("flir2tif", {
+            "path": os.path.join(sites_root, 'sites/ua-mac/Level_1/ir_geotiff/'),
+            "type": 'timestamp',
+            "parent": "flirIrCamera"}),
+        ("rulechecker", {
+            "type": "psql",
+            "query": "select count(distinct file_path) from extractor_ids where output like 'Full Field -- Thermal IR GeoTIFFs - %s%%';",
+            "parent": "flir2tif"}),
+        ("fullfield", {
+            "path": os.path.join(sites_root, 'sites/ua-mac/Level_1/fullfield/'),
+            "type": 'regex',
+            "regex": "^.*\d+_ir_.*thumb.tif"}),
+        ("meantemp", {
+            "path": os.path.join(sites_root, 'sites/ua-mac/Level_1/fullfield/'),
+            "type": 'regex',
+            "regex": '',
+            "parent": "fullfield"})
+    ])
+}
 
-PERCENT_COMPLETE_MINIMUM = 0.99
-
+MIN_PERCENT = 1
 MINIMUM_DATE_STRING = '2018-01-01'
 
 
@@ -32,112 +82,103 @@ def generate_dates_in_range(start_date_string):
         date_strings.append(current_date_string)
     return date_strings
 
-def update_csv_file_rulemonitor_rgb(path_to_file, dates_to_check):
-    df = pd.read_csv(path_to_file)
-    # all_dates = df['Date'].tolist()
+def perform_count(target_def, date, conn):
+    """Return count of specified type"""
 
-    indices = list(df.column.values)
+    count = 0
+    if target_def["type"] == "timestamp":
+        date_dir = os.path.join(target_def["path"], date)
+        if os.path.exists(date_dir):
+            count = len(os.listdir(date_dir))
 
-    conn = psycopg2.connect(dbname=dbname, user=user, host=host, password=password)
-    for current_date in dates_to_check:
-        if (df['Date'] == current_date).any():
-            output_string = 'Full Field -- RGB GeoTIFFs - ' + current_date + '%'
-            query = "select count(distinct file_path) from extractor_ids where output like '%s';"
-            query = query % output_string
-            curs = conn.cursor()
-            curs.execute(query)
-            results = []
-            for result in curs:
-                results.append(result)
-            value = int(results[0][0])
-            df.loc[df['Date'] == current_date, 'rgb ruleDB'] = value
-    df.to_csv(path_to_file, index=False)
+    elif target_def["type"] == "regex":
+        date_dir = os.path.join(target_def["path"], date)
+        if os.path.exists(date_dir):
+            for timestamp in os.listdir(date_dir):
+                ts_dir = os.path.join(date_dir, timestamp)
+                for file in os.listdir(ts_dir):
+                    if re.match(target_def["regex"], file):
+                        count += 1
 
-def update_csv_file_rulemonitor_ir_ruledb(path_to_file, dates_to_check):
-    df = pd.read_csv(path_to_file)
-    # all_dates = df['Date'].tolist()
+    elif target_def["type"] == "psql":
+        query_string = target_def["query"] % date
+        curs = conn.cursor()
+        curs.execute(query_string)
+        for result in curs:
+            count = result[0]
 
-    indices = list(df.column.values)
+    return count
 
-    conn = psycopg2.connect(dbname=dbname, user=user, host=host, password=password)
-    for current_date in dates_to_check:
-        if (df['Date'] == current_date).any():
-            # TODO change name
-            output_string = 'Full Field -- RGB GeoTIFFs - ' + current_date + '%'
-            query = "select count(distinct file_path) from extractor_ids where output like '%s';"
-            query = query % output_string
-            curs = conn.cursor()
-            curs.execute(query)
-            results = []
-            for result in curs:
-                results.append(result)
-            value = int(results[0][0])
-            df.loc[df['Date'] == current_date, 'rgb ruleDB'] = value
-    df.to_csv(path_to_file, index=False)
+def update_file_counts(sensor, dates_to_check, conn):
+    """Perform necessary counting to update CSV."""
 
-def update_csv_file_rgb_geotiff(path_to_file, dates_to_check):
+    output_file = sensor+"_PipelineWatch.csv"
+    print("Updating counts for %s into %s" % (sensor, output_file))
+    targets = count_defs[sensor]
 
-    df = pd.read_csv(path_to_file)
-    # all_dates = df['Date'].tolist()
+    # Load data frame from existing CSV or create a new one
+    if os.path.exists(output_file):
+        df = pd.read_csv(output_file)
+    else:
+        cols = ["date"]
+        for target_count in targets:
+            target_def = targets[target_count]
 
-    indices = list(df.column.values)
+            cols.append(target_count)
+            if "parent" in target_def:
+                cols.append(target_count+'%')
+
+        df = pd.DataFrame(columns=cols)
 
     for current_date in dates_to_check:
+        print("...scanning %s" % current_date)
+        counts = {}
+        percentages = {}
+
+        # Populate count and percentage (if applicable) for each target count
+        for target_count in targets:
+            target_def = targets[target_count]
+            counts[target_count] = perform_count(target_def, current_date, conn)
+            if "parent" in target_def:
+                if target_def["parent"] not in counts:
+                    counts[target_def["parent"]] = perform_count(targets[target_def["parent"]], current_date, conn)
+                if counts[target_def["parent"]] > 0:
+                    percentages[target_count] = float(counts[target_count]/counts[target_def["parent"]])
+                else:
+                    percentages[target_count] = 0
+        # If this date already has a row, just update
         if (df['Date'] == current_date).any():
-            percent_complete = float(df.loc[df['Date'] == current_date, 'bin2tif%'].values[0])
-            if percent_complete < PERCENT_COMPLETE_MINIMUM:
-                num_stereotop_files = len(os.listdir(stereotop_dir + current_date))
-                num_rgb_geotiff_files = len(os.listdir(rgb_geotiff_dir + current_date))
-                new_percent_complete = float(num_rgb_geotiff_files/num_stereotop_files)
-                if new_percent_complete > percent_complete:
-                    df.loc[df['Date'] == current_date, 'stereoTop'] = num_stereotop_files
-                    df.loc[df['Date'] == current_date, 'rgb_geotiff'] = num_rgb_geotiff_files
-                    df.loc[df['Date'] == current_date, 'bin2tif%'] = new_percent_complete
+            for target_count in targets:
+                target_def = targets[target_count]
+                df.loc[df['date'] == current_date, target_count] = counts[target_count]
+                if "parent" in target_def:
+                    df.loc[df['date'] == current_date, target_count+'%'] = percentages[target_count+'%']
+
+        # If not, create a new row
         else:
-            num_stereotop_files = len(os.listdir(stereotop_dir + current_date))
-            num_rgb_geotiff_files = len(os.listdir(rgb_geotiff_dir + current_date))
-            new_percent_complete = float(num_rgb_geotiff_files / num_stereotop_files)
-            new_entry = [current_date, num_stereotop_files, num_rgb_geotiff_files, new_percent_complete
-                         , '', '', '', '', '', '', '', '', '']
+            new_entry = [current_date]
+            indices = ["date"]
+
+            for target_count in targets:
+                target_def = targets[target_count]
+
+                indices.append(target_count)
+                new_entry.append(counts[target_count])
+                if "parent" in target_def:
+                    indices.append(target_count+'%')
+                    new_entry.append(percentages[target_count])
+
             df = df.append(pd.Series(new_entry, index=indices), ignore_index=True)
 
-    df.to_csv(path_to_file, index=False)
-
-
-def update_csv_file_ir_geotiff(path_to_file, dates_to_check):
-    df = pd.read_csv(path_to_file)
-    ## all_dates = df['Date'].tolist()
-
-    indices = list(df.column.values)
-
-
-    for current_date in dates_to_check:
-        if (df['Date'] == current_date).any():
-            percent_complete = float(df.loc[df['Date'] == current_date, 'flir2tif%'].values[0])
-            if percent_complete < PERCENT_COMPLETE_MINIMUM:
-                num_ir_files = len(os.listdir(ir_geotiff_dir + current_date))
-                num_ir_geotiff_files = len(os.listdir(ir_geotiff_dir + current_date))
-                new_percent_complete = float(num_ir_files/num_ir_geotiff_files)
-                if new_percent_complete > percent_complete:
-                    df.loc[df['Date'] == current_date, 'flirIrCamera'] = num_ir_files
-                    df.loc[df['Date'] == current_date, 'ir_geotiff'] = num_ir_geotiff_files
-                    df.loc[df['Date'] == current_date, 'flir2tif%'] = new_percent_complete
-        else:
-            num_ir_files = len(os.listdir(ir_geotiff_dir + current_date))
-            num_ir_geotiff_files = len(os.listdir(ir_geotiff_dir + current_date))
-            new_percent_complete = float(num_ir_files / num_ir_geotiff_files)
-            new_entry = [current_date, num_ir_files, num_ir_geotiff_files, new_percent_complete
-                , '', '', '', '', '', '', '', '', '']
-            df = df.append(pd.Series(new_entry, index=indices), ignore_index=True)
-    df.to_csv(path_to_file, index=False)
+    df.to_csv(output_file, index=False)
 
 
 def main():
+    conn = psycopg2.connect(dbname="rulemonitor", user="rulemonitor", host="192.168.5.169", password=SECRET)
     dates_in_range = generate_dates_in_range(MINIMUM_DATE_STRING)
 
-    update_csv_file_rgb_geotiff(check_table_csv, dates_in_range)
-    update_csv_file_ir_geotiff(check_table_csv, dates_in_range)
-    update_csv_file_rulemonitor_rgb(check_table_csv, dates_in_range)
+    for sensor in count_defs:
+        update_file_counts(sensor, dates_in_range, conn)
 
 if __name__ == '__main__':
     main()
