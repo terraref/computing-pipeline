2b57730376dbbaff0e85590daed1a4fac6e5c4ac
toddn <todd.nicholson@gmail.com>
2018-10-06 15:55:13 -0500

    parameter for choosing sensor added showall parameter for showing all, else shows the tail of last 14

--- scripts/checklist/api.py
+++ scripts/checklist/api.py
@@ -5,7 +5,13 @@ import os
 import json
 
 
+
+
 def create_app(test_config=None):
+
+    path_to_flir_csv = os.getenv("FLIR_IR_CAMERA_CSV", 'flirIrCamera_PipelineWatch.csv')
+    path_to_stereotop_csv = os.getenv("STEREOTOP_CSV", 'stereoTop_PipelineWatch.csv')
+
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
@@ -14,9 +20,11 @@ def create_app(test_config=None):
     )
 
     dataset = tablib.Dataset()
-    with open('CHECK_TABLE.csv') as f:
-        dataset.csv = f.read()
-    df = pd.read_csv('CHECK_TABLE.csv')
+    # with open('CHECK_TABLE.csv') as f:
+    #     dataset.csv = f.read()
+
+    df_flirIr = pd.read_csv(path_to_flir_csv)
+    df_stereoTop = pd.read_csv(path_to_stereotop_csv)
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
@@ -37,10 +45,21 @@ def create_app(test_config=None):
         return 'Hello, World! Saluton, Mondo!'
         # displays json string
 
-    @app.route('/showcsv')
-    def showcsv():
-        data = dataset.html
-        return df.to_html()
+    @app.route('/showcsv/<sensor_name>/<showall>')
+    def showcsv(sensor_name, showall):
+
+        # data = dataset.html
+
+        if showall.lower() == 'true':
+            if sensor_name.lower() == 'stereotop':
+                return df_stereoTop.to_html()
+            elif sensor_name.lower() == 'flirircamera':
+                return df_flirIr.to_html()
+        else:
+            if sensor_name.lower() == 'stereotop':
+                return df_stereoTop.tail(14).to_html()
+            elif sensor_name.lower() == 'flirircamera':
+                return df_flirIr.tail(14).to_html()
 
     return app
 
